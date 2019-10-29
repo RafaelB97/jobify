da56f832d942f75839fde18b46085edaa4a329a9
Maicher Oliver <om018@hdm-stuttgart.de>
2019-10-29 16:33:14 -0600

    added randomized db seeding with Faker gem in ruby

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,29 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   jobss = jobs.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', jobs: jobss.first)
+#
+#create_table "jobs", force: :cascade do |t|
+#  t.string "name"
+#  t.text "summary"
+#  t.text "description"
+#  t.decimal "payment"
+#  t.date "jobDate"
+#  t.datetime "created_at", null: false
+#  t.datetime "updated_at", null: false
+#
+
+jobs.destroy_all
+
+100.times do |index|
+  jobs.create!( name: Faker::Name.name,
+                summary: Faker::Lorem.sentence(3, false, 0).chop,
+                description: Faker::Lorem.paragraph,
+                payment: Faker::Number.between(1, 1000),
+                jobDate: Faker::Lorem.sentence(3, false, 0).chop,
+                created_at: Faker::Date.forward,
+                updated_at: Faker::Date.forward)
+end
+
+p "Created #{jobs.count} jobs"
