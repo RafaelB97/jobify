2d0b3d6498c4410d8fcbfc3fe07c37a5b27a1da6
Rafael <antoniorafael97@hotmail.com>
2019-11-05 07:20:17 -0600

    Try to change userType value when new user session created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,22 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # TODO Search why this work with config/routes.rb
+  def after_sign_in_path_for(resource)
+    # session[:domain_prefix] = current_user.domain_prefix
+    # user_path(resource)
+    puts "hola mundo"
+    puts session[:userType]
+    session[:userType] = current_user.userType
+    puts current_user.userType
+    stored_location_for(resource) || pages_home_path
+  end
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:firstName, :lastName, :birthday, :address, :phone, :userType])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:userType])
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,12 @@ Rails.application.routes.draw do
   get 'pages/faq'
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # TODO searching why this works with app/controllers/application_controller.rb
+  # TODO Search why if you delete after_sign_in_path_for from app/controllers/application_controller.rb
+  # this dosen't work at least you change ":user" to ":users"
+  
+  namespace :users do
+    root :to => 'pages#home'
+  end
 end
